c18bec589442fd91eba27707a821b730dd2820db
cheer <zhuxinjia.cheer@bytedance.com>
2023-10-02 22:00:18 +0800

    Initial Commit

---
+++ study_word/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  study_word
+//
+//  Created by ByteDance on 2023/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ study_word/study_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_wordApp.swift
+//  study_word
+//
+//  Created by ByteDance on 2023/10/2.
+//
+
+import SwiftUI
+
+@main
+struct study_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
